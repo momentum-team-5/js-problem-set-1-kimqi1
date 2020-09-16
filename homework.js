e3d1c0c891889841f100e88d0887baa80fa96755
Kim Leibowitz <kim@brightlightwellness.com>
2020-09-16 09:39:30 -0400

    1-7

--- homework.js
+++ homework.js
@@ -12,7 +12,7 @@ let leftover = 20 % 6
 function greeting(name){
     return "Hello, " + name + "!"
 }
-name("Chase")
+
 
 
 // 4. Create a function called isOdd that, given a number, will
@@ -31,13 +31,24 @@ function isOdd (number) {
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
 
+function isEven (number) { 
+    return number % 2 === 0
+}
+
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius (Temp) {
+
+    return (Temp - 32)* (5/9)
+}
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit (Temp) {
+    return (Temp * 1.8) + 32
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -46,9 +57,18 @@ function isOdd (number) {
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin (Temp)  {
+
+    return fahrenheitToCelsius(Temp)+ 273.15 
+
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser () {
+    let 
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
